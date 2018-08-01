2664576b9d52f3042967e04eedea596d2f5b2831
Luis Ulloa <timken@gmail.com>
2018-08-01 17:02:09 +0100

    Changed to new size of model

--- cape_document_qa/cape_document_qa_settings.py
+++ cape_document_qa/cape_document_qa_settings.py
@@ -6,7 +6,7 @@ MODELS_FOLDER = os.getenv('CAPE_MODELS_FOLDER', os.path.join(THIS_FOLDER, 'stora
 MODEL_FOLDER = os.getenv('CAPE_MODEL_FOLDER', os.path.join(MODELS_FOLDER, 'production_ready_model'))
 MODEL_URL = os.getenv('CAPE_MODEL_URL',
                       'https://github.com/bloomsburyai/cape-document-qa/releases/download/v0.1.2/production_ready_model.tar.xz')
-MODEL_MB_SIZE = os.getenv('CAPE_MODEL_MB_SIZE', 375)
+MODEL_MB_SIZE = os.getenv('CAPE_MODEL_MB_SIZE', 422)
 DOWNLOAD_ALL_GLOVE_EMBEDDINGS = os.getenv('DOWNLOAD_ALL_GLOVE_EMBEDDINGS', 'False').lower() == 'true'
 GLOVE_EMBEDDINGS_URL = os.getenv('CAPE_GLOVE_EMBEDDINGS_URL', 'https://nlp.stanford.edu/data/glove.840B.300d.zip')
 

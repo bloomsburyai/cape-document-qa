77ae3d3eec19a3189023d19fe21b54249524ec50
Patrick Lewis <patrick@bloomsbury.ai>
2018-08-17 17:34:51 +0100

    envint

--- cape_document_qa/cape_document_qa_settings.py
+++ cape_document_qa/cape_document_qa_settings.py
@@ -14,13 +14,17 @@
 
 import os
 
+def envint(varname: str, default: int) -> int:
+   return int(os.getenv(varname, default))
+
+
 THIS_FOLDER = os.path.abspath(os.path.join(os.path.dirname(__file__)))
 
 MODELS_FOLDER = os.getenv('CAPE_MODELS_FOLDER', os.path.join(THIS_FOLDER, 'storage', 'models'))
 MODEL_FOLDER = os.getenv('CAPE_MODEL_FOLDER', os.path.join(MODELS_FOLDER, 'production_ready_model'))
 MODEL_URL = os.getenv('CAPE_MODEL_URL',
                       'https://github.com/bloomsburyai/cape-document-qa/releases/download/v0.1.2/production_ready_model.tar.xz')
-MODEL_MB_SIZE = os.getenv('CAPE_MODEL_MB_SIZE', 422)
+MODEL_MB_SIZE = envint('CAPE_MODEL_MB_SIZE', 422)
 DOWNLOAD_ALL_GLOVE_EMBEDDINGS = os.getenv('DOWNLOAD_ALL_GLOVE_EMBEDDINGS', 'False').lower() == 'true'
 GLOVE_EMBEDDINGS_URL = os.getenv('CAPE_GLOVE_EMBEDDINGS_URL', 'https://nlp.stanford.edu/data/glove.840B.300d.zip')
 
